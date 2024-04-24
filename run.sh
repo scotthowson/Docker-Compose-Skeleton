72b858f0a602483f006aa7605621d2aa0bd141b7
Scott Howson <scott_howson@corruptiongames.net>
2024-04-24 00:37:32 -0400

    Updates

--- run.sh
+++ run.sh
@@ -7,12 +7,16 @@ BASE_DIR="CHANGEME/.docker-compose"
 LOG_FILE="CHANGEME/.docker-compose/Docker.log"
 
 # Start all services with specific .env files
-docker-compose --env-file $BASE_DIR/core/.env -f $BASE_DIR/core/docker-compose.yml \
-               --env-file $BASE_DIR/development/.env -f $BASE_DIR/development/docker-compose.yml \
-               --env-file $BASE_DIR/misc/.env -f $BASE_DIR/misc/docker-compose.yml \
-               --env-file $BASE_DIR/media/.env -f $BASE_DIR/media/docker-compose.yml \
-               --env-file $BASE_DIR/utilities/.env -f $BASE_DIR/utilities/docker-compose.yml \
-               --env-file $BASE_DIR/web/.env -f $BASE_DIR/web/docker-compose.yml \
+docker-compose --env-file $BASE_DIR/core-infrastructure/.env -f $BASE_DIR/core-infrastructure/docker-compose.yml \
+               --env-file $BASE_DIR/media-services/.env -f $BASE_DIR/media-services/docker-compose.yml \
+               --env-file $BASE_DIR/web-applications/.env -f $BASE_DIR/web-applications/docker-compose.yml \
+               --env-file $BASE_DIR/monitoring-management/.env -f $BASE_DIR/monitoring-management/docker-compose.yml \
+               --env-file $BASE_DIR/networking-security/.env -f $BASE_DIR/networking-security/docker-compose.yml \
+               --env-file $BASE_DIR/development-tools/.env -f $BASE_DIR/development-tools/docker-compose.yml \
+               --env-file $BASE_DIR/communication-collaboration/.env -f $BASE_DIR/communication-collaboration/docker-compose.yml \
+               --env-file $BASE_DIR/storage-backup/.env -f $BASE_DIR/storage-backup/docker-compose.yml \
+               --env-file $BASE_DIR/entertainment-personal/.env -f $BASE_DIR/entertainment-personal/docker-compose.yml \
+               --env-file $BASE_DIR/miscellaneous-services/.env -f $BASE_DIR/miscellaneous-services/docker-compose.yml \
                up -d
 
 # Check if Docker Compose succeeded
